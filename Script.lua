a8da493fc15d2bb2c8f1e3889cb7aa9a6844fdd9
IArepetsky <132691071+IArepetsky@users.noreply.github.com>
2023-08-16 16:07:28 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -96,3 +96,23 @@ local Button = FunTab:CreateButton({
         game.StarterGui:SetCore("SendNotification", {Title="Zero Gravity"; Text="Gravity Disabled"; Duration=1;})
     end,
  })
+
+local ToolsTab = Window:CreateTab("Tools", 4483362458)
+
+local Button = ToolsTab:CreateButton({
+    Name = "TPtool",
+    Callback = function()
+        local Tele = Instance.new("Tool", game.Players.LocalPlayer.Backpack)
+Tele.RequiresHandle = false
+Tele.RobloxLocked = true
+Tele.Name = "TPTool"
+Tele.ToolTip = "Teleport Tool"
+Tele.Equipped:connect(function(Mouse)
+	Mouse.Button1Down:connect(function()
+		if Mouse.Target then
+			game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name).HumanoidRootPart.CFrame = (CFrame.new(Mouse.Hit.x, Mouse.Hit.y + 5, Mouse.Hit.z))
+		end
+	end)
+end)
+    end,
+ })
